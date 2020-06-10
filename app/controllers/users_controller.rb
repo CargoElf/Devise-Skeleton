20afa6ba37c60c2605711e6a4e35ee46f445a584
CargoElf <benjamin.m.vansickle@gmail.com>
2020-06-10 14:43:03 -0500

    add basic nav bar and footer and logout button

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,11 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def index
+    @users = User.all
+  end
+end
